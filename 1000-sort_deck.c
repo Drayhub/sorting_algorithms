48da34317dd72944de37a1847951dcf8a094a5b9
Damilare Olawaye <olawayedamilare17@gmail.com>
2023-01-23 00:05:42 +0100

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,34 +1,56 @@
-#include "deck.h"
 #include <stdlib.h>
+#include "deck.h"
 
+/**
+ * cmp - Compare two cards based on their value and suit
+ * @a: First card
+ * @b: Second card
+ *
+ * Return: Negative value if a < b, positive value if a > b, 0 if a = b
+ */
 int cmp(const void *a, const void *b)
 {
-    const deck_node_t *n1 = a;
-    const deck_node_t *n2 = b;
-    int value_cmp = strcmp(n1->card->value, n2->card->value);
-    int kind_cmp = n1->card->kind - n2->card->kind;
+    const deck_node_t *node_a = *(const deck_node_t **)a;
+    const deck_node_t *node_b = *(const deck_node_t **)b;
+    int value_cmp = strcmp(node_a->card->value, node_b->card->value);
 
     if (value_cmp != 0)
         return value_cmp;
-    return kind_cmp;
+    return node_a->card->kind - node_b->card->kind;
 }
 
+/**
+ * sort_deck - Sort a deck of cards
+ * @deck: Pointer to the first node of the deck
+ */
 void sort_deck(deck_node_t **deck)
 {
-    deck_node_t *current = *deck;
-    size_t size = 52;
-    deck_node_t *nodes[52];
+    deck_node_t *current;
+    deck_node_t **array;
+    size_t size, i;
 
-    // Copy the nodes to an array
-    for (size_t i = 0; i < size; i++)
+    if (!deck || !*deck)
+        return;
+    current = *deck;
+    size = 0;
+    while (current)
     {
-        nodes[i] = current;
+        size++;
         current = current->next;
     }
-
-    // Sort the array of nodes
-    qsort(nodes, size, sizeof(deck_node_t *), cmp);
-
-    // Reconnect the sorted nodes
-    *deck = nodes[0];
-    nodes[0]->prev = NULL;
+    array = malloc(sizeof(deck_node_t *) * size);
+    if (!array)
+        return;
+    current = *deck;
+    i = 0;
+    while (current)
+    {
+        array[i] = current;
+        current = current->next;
+        i++;
+    }
+    qsort(array, size, sizeof(deck_node_t *), cmp);
+    for (i = 0; i < size; i++)
+    {
+        array[i]->prev = i > 0 ? array[i - 1] : NULL;
+       
