4befa9f7aeb224b950062f3640a92afc21bd5b6d
Damilare Olawaye <olawayedamilare17@gmail.com>
2023-01-22 23:40:00 +0100

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -75,10 +75,27 @@ void sorting_hoare(int *array, int first, int last, size_t size)
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	int last = size - 1;
-
-	if (!array || size <= 1)
-		return;
-
-	sorting_hoare(array, 0, last, size);
+    if (size < 2)
+        return;
+    int pivot = array[size - 1];
+    int i = -1, j = size;
+    while (1)
+    {
+        do
+            i++;
+        while (array[i] < pivot);
+        do
+            j--;
+        while (array[j] > pivot);
+        if (i >= j)
+            break;
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        for (int k = 0; k < size; k++)
+            printf("%d ", array[k]);
+        printf("\n");
+    }
+    quick_sort_hoare(array, j);
+    quick_sort_hoare(array + j, size - j);
 }
